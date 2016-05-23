0236577566892042669a017477a3d684b56ef4ca
Andrew Start <start.andrew@gmail.com>
2016-05-23 10:12:22 -0400

    Updated to reflect changed API and what is actually exported from the module.

--- fb.d.ts
+++ fb.d.ts
@@ -1,18 +1,29 @@
-declare class FB {
-    api(path:string, callback?:(response:any) => void):void;
-    api(path:string, params:any, callback?:(response:any) => void):void;
-    api(path:string, method:string, params:any, callback?:(response:any) => void):void;
-    napi(path:string, callback?:(error:any, response:any) => void):void;
-    napi(path:string, params:any, callback?:(error:any, response:any) => void):void;
-    napi(path:string, method:string, params:any, callback?:(error:any, response:any) => void):void;
+/// <reference path="./es6-promise.d.ts" />
+export class Facebook {
+    api(path: string): Promise<any>;
+    api(path: string, callback: (response: any) => void): void;
+    api(path: string, params: any): Promise<any>;
+    api(path: string, params: any, callback: (response: any) => void): void;
+    api(path: string, method: string, params: any): Promise<any>;
+    api(path: string, method: string, params: any, callback: (response: any) => void): void;
+    napi(path: string, callback?: (error: any, response: any) => void): void;
+    napi(path: string, params: any, callback?: (error: any, response: any) => void): void;
+    napi(path: string, method: string, params: any, callback?: (error: any, response: any) => void): void;
     getAccessToken(): string;
-    setAccessToken(token:string): void;
-    parseSignedRequest(signedRequest:string, appSecret?:string): any;
-    getLoginUrl(options:any): string;
+    setAccessToken(token: string): void;
+    parseSignedRequest(signedRequest: string, appSecret?: string): any;
+    getLoginUrl(options: any): string;
     options(): any;
-    options(optionName:string): any;
-    options(optionsDict:any): void;
-    version: string;
+    options(optionName: string): any;
+    options(optionsDict: any): void;
 }
 
-export = new FB
+export default new Facebook;
+export var FB: Facebook;
+export var version: string;
+
+export class FacebookApiException {
+    name: string;
+    message: string;
+    response: any;
+}
