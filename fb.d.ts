fe11fc426cbb16b6589ac90e6931d6df96a52220
Andrew Start <start.andrew@gmail.com>
2016-06-12 12:29:50 -0400

    Added missing methods, some option definitions.

--- fb.d.ts
+++ fb.d.ts
@@ -1,22 +1,53 @@
 import Promise = require("any-promise");
 
+type HTTPMethods = "get" | "post" | "delete";
+
+export interface FacebookOptions {
+    accessToken?: string;
+    appId?: string;
+    appSecret?: string;
+    version?: string;
+    proxy?: string;
+    timeout?: number;
+    scope?: string;
+    redirectUri?: string;
+    Promise?: any;
+}
+
+export interface LoginUrlOptions {
+    appId?: string;
+    client_id?: string;
+    redirectUri?: string;
+    redirect_uri?: string;
+    scope?: string;
+    display?: string;
+    state?: string;
+    responseType?: string;
+    response_type?: string;
+}
+
 export class Facebook {
     api(path: string): Promise<any>;
     api(path: string, callback: (response: any) => void): void;
     api(path: string, params: any): Promise<any>;
     api(path: string, params: any, callback: (response: any) => void): void;
-    api(path: string, method: string, params: any): Promise<any>;
-    api(path: string, method: string, params: any, callback: (response: any) => void): void;
+    api(path: string, method: HTTPMethods): Promise<any>;
+    api(path: string, method: HTTPMethods, callback: (response: any) => void): void;
+    api(path: string, method: HTTPMethods, params: any): Promise<any>;
+    api(path: string, method: HTTPMethods, params: any, callback: (response: any) => void): void;
     napi(path: string, callback?: (error: any, response: any) => void): void;
     napi(path: string, params: any, callback?: (error: any, response: any) => void): void;
-    napi(path: string, method: string, params: any, callback?: (error: any, response: any) => void): void;
+    napi(path: string, method: HTTPMethods, callback?: (error: any, response: any) => void): void;
+    napi(path: string, method: HTTPMethods, params: any, callback?: (error: any, response: any) => void): void;
     getAccessToken(): string;
     setAccessToken(token: string): void;
     parseSignedRequest(signedRequest: string, appSecret?: string): any;
-    getLoginUrl(options: any): string;
-    options(): any;
+    getLoginUrl(options?: LoginUrlOptions): string;
+    options(): FacebookOptions;
     options(optionName: string): any;
-    options(optionsDict: any): void;
+    options(optionsDict: FacebookOptions): void;
+    extend(options: FacebookOptions): Facebook;
+    withAccessToken(token: string): Facebook;
 }
 
 export default new Facebook;
