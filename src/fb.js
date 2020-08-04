be4ac3be24a6b026687350473cba00d7a528e384
Daniel Bruness <daniel.bruness@kite.thm.de>
2020-08-04 20:55:20 +0200

    Added graph-video option

--- src/fb.js
+++ src/fb.js
@@ -26,6 +26,7 @@ var {version} = require('../package.json'),
 		appSecret: null,
 		appSecretProof: null,
 		beta: false,
+		graphVideo: false,
 		version: 'v2.5',
 		timeout: null,
 		scope: null,
@@ -358,7 +359,7 @@ class Facebook {
 		if ( !/^v\d+\.\d+\//.test(path) ) {
 			path = this.options('version') + '/' + path;
 		}
-		uri = `https://graph.${this.options('beta') ? 'beta.' : ''}facebook.com/${path}`;
+		uri = `https://graph${this.options('graphVideo') ? '-video' : ''}.${this.options('beta') ? 'beta.' : ''}facebook.com/${path}`;
 
 		parsedUri = URL.parse(uri);
 		delete parsedUri.search;
